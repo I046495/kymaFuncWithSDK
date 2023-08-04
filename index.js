611c1a7277a81db04f744b9b0abb2b06baf85fbf
I046495 <ashish.bansal01@sap.com>
2023-08-04 11:26:04 +0200

    commitffirst

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const oDataBasePrice = require("./OData/BasePrices")
 module.exports = {
 	main: async function (event, context) {
-        const response = await getBasePrices();
+        const response = await getBasePrices1();
         
     return response;
     }
 }
 
-	async function getBasePrices() {
+	async function getBasePrices1() {
     	return oDataBasePrice.requestBuilder().getAll().execute({ destinationName: 'myDestinationName' });
     }
