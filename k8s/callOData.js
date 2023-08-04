c30561174d5c35bfe15c829f29cdad8613bf4463
I046495 <ashish.bansal01@sap.com>
2023-08-04 14:02:24 +0200

    basic func

--- k8s/callOData.js
+++ k8s/callOData.js
@@ -1,12 +1,25 @@
-const oDataBasePrice = require("./OData/BasePrices")
 module.exports = {
-	main: async function (event, context) {
-        const response = await getBasePrices();
-        
-    return response;
-    }
-}
+   main: function (event, context) {
+ 
+     var reqUrl = event.extensions.request.originalUrl;
+     var reqPath = event.extensions.request.path;
+ 
+     console.log("Request url / url [" + reqUrl + "] pathname [" + reqPath+ "] received.");
+ 
+     var promo = event.data;
+     console.log(JSON.stringify(promo));
+     
+     let messages = new Array();
+     if (promo.promoID == "") {
+       messages[0] = "1";
+       messages[1] = "11";
+     }
+     else {
+      messages[0] = "2";
+      messages[1] = "22";
+     }
 
-	async function getBasePrices() {
-    	return oDataBasePrice.requestBuilder().getAll().execute({ destinationName: 'myDestinationName' });
-    }
+ 
+     return JSON.stringify(messages);
+   }
+}
