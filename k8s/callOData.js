e353f9459e53bbc829f4fcf0a1b359cc4748a7c7
sap-andreas-paul <45387398+sap-andreas-paul@users.noreply.github.com>
2023-08-11 14:43:06 +0200

    Update callOData.js

--- k8s/callOData.js
+++ k8s/callOData.js
@@ -2,28 +2,28 @@ module.exports = {
    main: function (event, context) {
 
       if(true) {
-         return "Hello!";
+         return "Hello!!!";
       } else {
-     var reqUrl = event.extensions.request.originalUrl;
-     var reqPath = event.extensions.request.path;
+//     var reqUrl = event.extensions.request.originalUrl;
+//     var reqPath = event.extensions.request.path;
  
-     console.log("Request url / url [" + reqUrl + "] pathname [" + reqPath+ "] received.");
+//     console.log("Request url / url [" + reqUrl + "] pathname [" + reqPath+ "] received.");
  
-     var promo = event.data;
-     console.log(JSON.stringify(promo));
+//     var promo = event.data;
+//     console.log(JSON.stringify(promo));
      
-     let messages = new Array();
-     if (promo.promoID == "") {
-       messages[0] = "1";
-       messages[1] = "11";
-     }
-     else {
-      messages[0] = "2";
-      messages[1] = "22";
-     }
+//     let messages = new Array();
+//     if (promo.promoID == "") {
+//       messages[0] = "1";
+//       messages[1] = "11";
+//     }
+//     else {
+//      messages[0] = "2";
+//      messages[1] = "22";
+//     }
 
  
-     return JSON.stringify(messages);
+//     return JSON.stringify(messages);
       }
    }
 }
