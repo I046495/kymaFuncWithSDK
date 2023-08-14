489c50417acccd355f764b4a958387cba23c31bf
Andreas Paul <andreas.paul@sap.com>
2023-08-14 11:32:19 +0200

    here we go

--- k8s/handler.js
+++ k8s/handler.js
@@ -1,5 +1,22 @@
 module.exports = {
    main: function (event, context) {
+		console.log("Here we go"); 
          return "Hello :-)!!!";
+         console.log("Here we went"); 
    }
 }
+
+
+//const oDataBasePrice = require("./OData/BasePrices")
+//module.exports = {
+//	main: async function (event, context) {
+//		console.log("Here we go");
+//        const response = await getBasePrices1();
+//    return response;
+//    }
+//
+//}
+//
+//	async function getBasePrices1() {
+//    	return oDataBasePrice.requestBuilder().getAll().execute({ destinationName: 'myDestinationName' });
+//    }
