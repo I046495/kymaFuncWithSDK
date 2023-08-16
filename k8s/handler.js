71a02b564587b4239a742d4aa3b618e922145d6b
Andreas Paul <andreas.paul@sap.com>
2023-08-16 14:15:57 +0200

    so

--- k8s/handler.js
+++ k8s/handler.js
@@ -7,32 +7,12 @@
 //
 
 const all = require("./OData");
-//const oData = require("./OData/service");
-//import { myEntityService } from './OData';
 
 module.exports = {
 	main: async function (event, context) {
-//		console.log("all:");
-//		console.log(all);
-//		console.log("all.oData:");
 		const od = all.oData;
-//		console.log(od);
-//		console.log("all.BasePricesRequestBuilder:");
-//		const bprb = all.BasePricesRequestBuilder;
-//		console.log(bprb);
-//		console.log("od()");
 		const odfn = od();
-//		console.log(odfn);
-		console.log("odfn.basePricesApi");
-		console.log(odfn.basePricesApi)
-		console.log("odfn.basePricesApi.requestBuilder()");
-		console.log(odfn.basePricesApi.requestBuilder());
-//		console.log("bprb()");
-//		console.log(bprb());
-//		const bp = all.BasePricesRequestBuilder();
-//		console.log("Step 3");
-		const resultPromise = odfn.basePricesApi.requestBuilder().getAll().top(5).execute({ destinationName: 'myDestinationName' });
-//        console.log("Step 4");
+		const resultPromise = odfn.basePricesApi.requestBuilder().getAll().top(5).execute({ destinationName: 'Kyma_Cf_Test_Destination' });
     return "done";
     }
 
