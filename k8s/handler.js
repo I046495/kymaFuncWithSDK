97e5f9a2f4eb39df0770fe1a1d7ff36ee12b4a64
Andreas Paul <andreas.paul@sap.com>
2023-08-16 13:30:21 +0200

    so

--- k8s/handler.js
+++ k8s/handler.js
@@ -12,18 +12,25 @@ const oData = require("./OData/service");
 
 module.exports = {
 	main: async function (event, context) {
-		console.log("Step 1");
+		console.log("all:");
 		console.log(all);
-		const od = all.oData();
-		const bp = all.BasePricesRequestBuilder();
-		const { basePriceApi } = all.oData();
-		console.log(basePriceApi);
-		console.log("Step 2");
-//		const basePricesApi = oData.basePricesApi;
-//		const basePricesApi = myEntityService();
-		console.log("Step 3");
-		const resultPromise = bp.getAll().top(5).execute({ destinationName: 'myDestinationName' });
-        console.log("Step 4");
+		console.log("all.oData:");
+		const od = all.oData;
+		console.log(od);
+		console.log("all.BasePricesRequestBuilder:");
+		const bprb = all.BasePricesRequestBuilder;
+		console.log(bprb);
+		console.log("od()");
+		const odfn = od();
+		console.log(odfn);
+		console.log("odfn.basePricesApi");
+		console.log(odfn.basePricesApi)
+//		console.log("bprb()");
+//		console.log(bprb());
+//		const bp = all.BasePricesRequestBuilder();
+//		console.log("Step 3");
+//		const resultPromise = bp.getAll().top(5).execute({ destinationName: 'myDestinationName' });
+//        console.log("Step 4");
     return "done";
     }
 
