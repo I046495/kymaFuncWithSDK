777b5c845dc51e258fdaf520ea6b0ecbbf0d7c1f
Andreas Paul <andreas.paul@sap.com>
2023-08-16 10:42:59 +0200

    so

--- k8s/handler.js
+++ k8s/handler.js
@@ -6,17 +6,21 @@
 //}
 //
 
-//const oData = require("./OData/service");
-import { myEntityService } from './OData';
+const all = require("./OData");
+const oData = require("./OData/service");
+//import { myEntityService } from './OData';
 
 module.exports = {
 	main: async function (event, context) {
 		console.log("Step 1");
-//		const basePricesApi = oData.basePricesApi;
-		const basePricesApi = myEntityService();
+		console.log(all);
+		console.log(oData);
 		console.log("Step 2");
+//		const basePricesApi = oData.basePricesApi;
+//		const basePricesApi = myEntityService();
+		console.log("Step 3");
 		const resultPromise = basePricesApi().requestBuilder().getAll().top(5).execute({ destinationName: 'myDestinationName' });
-        console.log("Step 3");
+        console.log("Step 4");
     return "done";
     }
 
