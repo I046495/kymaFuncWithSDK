89097ca1b7fc3666d3efd0dc10dec0e1526a5432
Andreas Paul <andreas.paul@sap.com>
2023-08-28 15:37:05 +0200

    wip

--- k8s/handler.js
+++ k8s/handler.js
@@ -12,8 +12,11 @@ module.exports = {
 	main: async function (event, context) {
 		const od = all.oData;
 		const odfn = od();
+		console.log("Lets go!");
 		const resultPromise = odfn.basePricesApi.requestBuilder().getAll().top(5).execute({ destinationName: 'PAULAN_ODATA_DEST' });
-    return "done";
+		console.log("Still alive!");
+		resultPromise.then(data => { console.log(data); } );
+		return "done";
     }
 
 }
