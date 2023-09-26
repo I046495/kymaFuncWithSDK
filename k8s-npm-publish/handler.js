1b8f2f95354e91b19dd9283f5e5e850652fe8983
Germain Girndt <germain.girndt@sap.com>
2023-09-26 17:52:36 +0200

    Deleted unused files

--- k8s-npm-publish/handler.js
+++ k8s-npm-publish/handler.js
@@ -1,29 +1,20 @@
-//module.exports = {
-//   main: function (event, context) {
-//		console.log("Here we go"); 
-//         return "Hello :-)!!!";
-//   }
-//}
-//
-
-
 const all = require("@sap-opps/odata-sdk");
 
 module.exports = {
-	main: async function (event, context) {
-		const od = all.oData;
-		const odfn = od();
-		console.log("Lets go!");
-		const resultPromise = odfn.basePricesApi.requestBuilder().getAll().top(5).execute({ destinationName: 'PAULAN_ODATA_DEST' });
+  main: async function (event, context) {
+    const od = all.oData;
+    const odfn = od();
+    console.log("Lets go!");
+    const resultPromise = odfn.basePricesApi.requestBuilder().getAll().top(5).execute({ destinationName: 'PAULAN_ODATA_DEST' });
 		console.log("Still alive!");
-		resultPromise.then(data => { console.log(data); } );
+    resultPromise.then(data => { console.log(data); } );
 		return "done";
-    }
+  }
 
 }
 
 
-// Uncomment for testing 
+// Uncomment for testing
 //const util = require('util');
 //console.log(util.inspect(all)); // string representation of the imported module
 // module.exports.main()
